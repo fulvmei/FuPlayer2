a18e7ef04f076684f24ceca0c536106e93867bb3
fulvmei <fulvmei@qq.com>
2022-07-26 17:22:56 +0800

    Initial commit

---
+++ demo/src/main/java/com/chengfu/android/fuplayer2/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.chengfu.android.fuplayer2.demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
